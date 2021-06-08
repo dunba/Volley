92fac96abecca4d93e104ee903d654ae2d9cc625
Dunba <dunbatehinse@yahoo.com>
2021-06-07 21:37:32 -0700

    more changes

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,6 +3,8 @@ import ShareIcon from '@material-ui/icons/Share';
 import CommentIcon from '@material-ui/icons/Comment';
 
 import '../feed.css'
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 
 
@@ -42,6 +44,21 @@ const Sidebar = ({ currentvid, onFastforward, goalvids }) => {
             console.log('hiding likes')
         }
     }
+    //this section handles the like button on the side and top panel
+    const [isliked, setIsLiked] = useState(false);
+    // this function handles the state once the like button is pressed
+    const likeHandler = (e) => {
+
+        if (isliked) {
+            setIsLiked(false);
+            console.log('hey')
+        } else {
+            setIsLiked(true);
+            console.log(e)
+            console.log('hey')
+        }
+    }
+
 
     return (
         <div>
@@ -51,19 +68,19 @@ const Sidebar = ({ currentvid, onFastforward, goalvids }) => {
                     <h3>{currentvid.team}</h3>
                     <p>250 Views</p>
                     <p onClick={showLikes}> <strong>{currentvid.likes.likedby.length}</strong> Likes</p>
-                    {islikesvisible ? <p>Liked by <strong>{currentvid.likes.likedby[Math.floor(Math.random() * 8)]}</strong> and <strong>{currentvid.likes.likedby.length}</strong> others </p> : ''}
+                    {islikesvisible ? <p>Liked by <strong>{currentvid.likes.likedby[Math.floor(Math.random() * 8)]}</strong> and <strong>{currentvid.likes.likedby.length - 1}</strong> others </p> : ''}
                 </div>
 
                 <div className='sidebar_icons'>
 
                     <div className='social_controls'>
-
+                        {isliked ? <FavoriteIcon id='iconn' onClick={likeHandler} fontSize='large' /> : <FavoriteBorderIcon onClick={likeHandler} id='iconn' fontSize='large' />}
                         <ShareIcon id='iconn' fontSize='large' />
                         <CommentIcon id='iconn' fontSize='large' onClick={commentHandler} />
                     </div>
                     <div className='commentssection'>
                         <strong>{currentvid.comments.length}</strong> Comments
-                        {iscommentvisible ? <p>{currentvid.comments.map(comment => (<p><strong>{comment.name}.      </strong>{comment.posting}<hr />                         </p>
+                        {iscommentvisible ? <p>{currentvid.comments.map(comment => (<div className='comments'><p><strong>{comment.name}.      </strong>{comment.posting}<hr />                         </p></div>
 
                         ))} <div>Add Commment...
                             <input ref={commentRef} type='text'></input>
