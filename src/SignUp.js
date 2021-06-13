8e42102346b54ac1d43b999352752706665b7759
Dunba <dunbatehinse@yahoo.com>
2021-06-13 13:17:52 -0700

    sign up page now pushes to '/setuserinfo'

--- src/SignUp.js
+++ src/SignUp.js
@@ -30,7 +30,7 @@ const SignUp = () => {
       setError('')
       setLoading(true)
       await signup(emailRef.current.value, passwordRef.current.value)
-      history.push('/')
+      history.push('/setuserinfo')
     } catch {
       setError('Failed to create an account')
     }
