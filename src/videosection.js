c07211cf8d2311f1de66bf16dd9c08f036eaab42
Dunba <dunbatehinse@yahoo.com>
2021-06-29 15:15:33 -0700

    framer motion, useinView hook

--- src/videosection.js
+++ src/videosection.js
@@ -1,79 +1,92 @@
 import React, { useState, useEffect } from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 import { Link } from "react-router-dom";
-import VisibilityIcon from "@material-ui/icons/Visibility";
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ChatIcon from "@material-ui/icons/Chat";
 import "./feed.css";
 import Carousel from "react-elastic-carousel"
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useAnimation } from "framer-motion";
 
 const Videosection = ({ type, servervideos, sectiontitle, serverpics }) => {
 
-
-  if (type == 'Recommended') return (<>
-    <h6>{type}</h6>
-
-
-    <div className="videosection2">
-      {servervideos.map(pic => (
-        <div className="vidposting2">
-          <Link to={`/watch/${pic.id}`}>
-            <img
-              key={pic.id}
-              alt={pic.description}
-              id="thumbnailonfeed"
-              src={pic.thumbnail}
-            />
-          </Link>
-          <div className="videodescription">{pic.description}</div>
-        </div>
-      ))}{" "}
-    </div>
-
-
-
-  </>)
-
-  if (!servervideos) return <ClipLoader />
-
-  else if (servervideos.length === 0) return (
-    <>
-      <div className="videosection">
-        <h6>{sectiontitle}</h6>
-
-        <span>No {sectiontitle} Videos </span>
-      </div>
-    </>
-  )
-  else if (sectiontitle === 'Liked') return (
-    <div className="videosection">
-      <h6>{sectiontitle}</h6>
-
-
-      <div className="reelholder">
-        {servervideos.map(pic => (
-          <div className="vidposting">
-            {servervideos.length} liked video in server
-            {/* <Link to={`/watch/${pic.id}`}>
-              <img
-                key={pic.id}
-                alt={pic.description}
-                id="thumbnailonfeed"
-                src={pic.thumbnail}
-              />
-            </Link> */}
-            <div className="videodescription">{pic.description}</div>
-          </div>
-        ))}{" "}
-      </div>
+  const { ref, inView } = useInView({ threshold: 0.15 });
+  const animation = useAnimation();
+  // if (type == 'Recommended') return (<>
+  //   <h6>{type}</h6>
+  //   <div className="videosection2">
+  //     {servervideos.map(pic => (
+  //       <div className="vidposting2">
+  //         <Link to={`/watch/${pic.id}`}>
+  //           <img
+  //             key={pic.id}
+  //             alt={pic.description}
+  //             id="thumbnailonfeed"
+  //             src={pic.thumbnail}
+  //           />
+  //         </Link>
+  //         <div className="videodescription">{pic.description}</div>
+  //       </div>
+  //     ))}{" "}
+  //   </div>
 
 
 
-    </div>
+  // </>)
 
-  )
+  // if (!servervideos) return <ClipLoader />
+
+  // else if (servervideos.length === 0) return (
+  //   <>
+  //     <div className="videosection">
+  //       <h6>{sectiontitle}</h6>
+
+  //       <span>No {sectiontitle} Videos </span>
+  //     </div>
+  //   </>
+  // )
+  // else if (sectiontitle === 'Liked') return (
+  //   <div className="videosection">
+  //     <h6>{sectiontitle}</h6>
+
+
+  //     <div className="reelholder">
+  //       {servervideos.map(pic => (
+  //         <div className="vidposting">
+  //           {servervideos.length} liked video in server
+  //           {/* <Link to={`/watch/${pic.id}`}>
+  //             <img
+  //               key={pic.id}
+  //               alt={pic.description}
+  //               id="thumbnailonfeed"
+  //               src={pic.thumbnail}
+  //             />
+  //           </Link> */}
+  //           <div className="videodescription">{pic.description}</div>
+  //         </div>
+  //       ))}{" "}
+  //     </div>
+
+
+
+  //   </div>
+
+
+  // )
+  useEffect(() => {
+    console.log('use effect hook, inView=', inView);
+    if (inView) {
+      animation.start({
+        x: 0,
+        opacity: 1,
+        transition: {
+          type: 'spring', duration: 1, bounce: 0.3
+        }
+      })
+    }
+    if (!inView) {
+      animation.start({ opacity: 0.3 })
+    }
+  }, [inView])
 
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
@@ -83,16 +96,18 @@ const Videosection = ({ type, servervideos, sectiontitle, serverpics }) => {
 
   ]
 
+
+
   return (
     <>
-      <div className="videosection">
+      <div className="videosection" ref={ref}>
         <h6>{sectiontitle}</h6>
-        <div className="reelholder">
+        <motion.div className="reelholder" animate={animation} >
 
           <Carousel breakPoints={breakPoints}>
             {servervideos.map(pic => (
-              // className = "vidposting"
-              <motion.div whileHover={{ scale: 1.2 }}>
+
+              <div className="vidposting">
                 <Link to={`/watch/${pic.id}`}>
                   <img
                     key={pic.id}
@@ -102,13 +117,13 @@ const Videosection = ({ type, servervideos, sectiontitle, serverpics }) => {
                   />
                 </Link>
                 <div className="videodescription">{pic.description}</div>
-              </motion.div>
+              </div>
             ))}
 
           </Carousel>
 
 
-        </div>
+        </motion.div>
 
 
       </div>
